991d6ea9efabfb5ffe3be933d8be2c74725cec45
James Millner <jamesmillnerdev@gmail.com>
2023-07-17 22:52:39 +0100

    OAuth authorize flow working up to step 3 in Wahoo's documentation. Need to figure out how to get the final exchange to work.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,10 @@ import org.http4k.filter.DebuggingFilters
 import org.http4k.filter.ServerFilters
 import org.http4k.routing.bind
 import org.http4k.routing.routes
+import org.http4k.server.Http4kServer
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
+import java.util.concurrent.Semaphore
 
 data class AuthorizationResponse(val code: String, val state: String)
 
@@ -14,12 +16,9 @@ data class AuthorizationResponse(val code: String, val state: String)
 private val logger = KotlinLogging.logger {}
 
 fun main() {
-
-
-    val wahooClientId = "<Your Wahoo Client ID>"
-    val wahooClientSecret = "<Your Wahoo Client Secret>"
-    val redirectUri = "<Your Redirect URI>"
-    val webhookUrl = "<Your Webhook URL>"
+    val wahooClientId = "***REMOVED***"
+    val wahooClientSecret = "***REMOVED***"
+    val redirectUri = "https://localhost:8000"
 
     val client = ApacheClient()
     val stateMap = mutableMapOf<String, String>()
@@ -27,29 +26,17 @@ fun main() {
     val app: HttpHandler = routes(
         "/authorize" bind Method.GET to { request: Request ->
             val state = request.query("state")
-            val authorizationUrl = "https://api.wahoofitness.com/v1/oauth/authorize?" +
+            val authorizationUrl = "https://api.wahooligan.com/oauth/authorize?" +
                     "client_id=$wahooClientId" +
-                    "&response_type=code" +
                     "&redirect_uri=$redirectUri" +
-                    "&state=$state"
+                    "&scope=user_read%20workouts_read%20offline_data" +
+                    "&response_type=code"
             Response(Status.TEMPORARY_REDIRECT).header("Location", authorizationUrl)
         },
 
-        "/callback" bind Method.GET to { request: Request ->
+        "/" bind Method.GET to { request: Request ->
             val code = request.query("code") ?: error("Authorization code not received")
-            val state = request.query("state") ?: error("State not received")
-            stateMap[code] = state
             Response(Status.OK).body("Authorization code received: $code")
-        },
-
-        "/webhook" bind Method.POST to { request: Request ->
-            val body = request.bodyString()
-            // Process the received webhook payload
-            // Implement your logic here
-
-            logger.info { body }
-
-            Response(Status.OK)
         }
     )
 
@@ -65,49 +52,23 @@ fun main() {
     println("Open the following URL in your browser to authorize the application:")
     println(authorizationRequestUrl)
 
-    // Wait for authorization code
-    val authorizationCode = waitForAuthorizationCode()
-    val state = stateMap.remove(authorizationCode)
-        ?: error("Invalid or expired authorization code received")
+    server.shutdownOnExit()
 
-    // Exchange authorization code for an access token
-    val tokenRequest = Request(Method.POST, "https://api.wahoofitness.com/v1/oauth/token")
-        .query("client_id", wahooClientId)
-        .query("client_secret", wahooClientSecret)
-        .query("grant_type", "authorization_code")
-        .query("code", authorizationCode)
-        .query("redirect_uri", redirectUri)
-
-    val tokenResponse = client(tokenRequest)
-
-    if (tokenResponse.status.successful) {
-        // Access token retrieval successful
-        println("Access token retrieved successfully")
-
-        // Register webhook URL
-        val registerWebhookRequest = Request(Method.POST, "https://api.wahoofitness.com/v1/webhooks")
-            .header("Authorization", "Bearer ${tokenResponse.bodyString()}")
-            .body("""{"url": "$webhookUrl"}""")
-
-        val registerWebhookResponse = client(registerWebhookRequest)
-
-        if (registerWebhookResponse.status.successful) {
-            // Webhook URL registered successfully
-            println("Webhook URL registered successfully: $webhookUrl")
-        } else {
-            // Handle error during webhook URL registration
-            println("Failed to register webhook URL: ${registerWebhookResponse.status}")
-        }
-    } else {
-        // Handle error during access token retrieval
-        println("Failed to retrieve access token: ${tokenResponse.status}")
-    }
-
-    server.stop()
+    waitForExitSignal()
 }
 
-private fun waitForAuthorizationCode(): String {
-    // Implement your code to wait for the authorization code
-    // For example, you can use a blocking read from the console
-    return readLine().orEmpty()
+private fun Http4kServer.shutdownOnExit() {
+    Runtime.getRuntime().addShutdownHook(Thread {
+        println("Shutting down...")
+
+        stop()
+
+        println("Shutdown complete.")
+    })
+}
+
+private fun waitForExitSignal() {
+    val semaphore = Semaphore(0)
+    Runtime.getRuntime().addShutdownHook(Thread { semaphore.release() })
+    semaphore.acquire()
 }
