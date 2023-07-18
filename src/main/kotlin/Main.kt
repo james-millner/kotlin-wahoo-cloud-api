9a9fc0d7e8a1a1a832830c40bf8df7d23bdc0cab
James Millner <jamesmillnerdev@gmail.com>
2023-07-18 23:04:05 +0100

    Bare basic HTTP4K Kotlin Wahoo Cloud API implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,8 +10,6 @@ import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 import java.util.concurrent.Semaphore
 
-data class AuthorizationResponse(val code: String, val state: String)
-
 // Place definition above class declaration to make field static
 private val logger = KotlinLogging.logger {}
 
@@ -21,7 +19,6 @@ fun main() {
     val redirectUri = System.getenv("REDIRECT_URI") ?: error("REDIRECT_URI environment variable not set")
 
     val client = ApacheClient()
-    val stateMap = mutableMapOf<String, String>()
 
     val app: HttpHandler = routes(
         "/authorize" bind Method.GET to { request: Request ->
@@ -36,7 +33,23 @@ fun main() {
 
         "/" bind Method.GET to { request: Request ->
             val code = request.query("code") ?: error("Authorization code not received")
-            Response(Status.OK).body("Authorization code received: $code")
+            logger.info { "Authorization code received: $code" }
+            // Need to do a POST here
+            val url = Uri.of("https://api.wahooligan.com/oauth/token?" +
+                    "client_id=$wahooClientId" +
+                    "&client_secret=$wahooClientSecret" +
+                    "&code=$code" +
+                    "&grant_type=authorization_code" +
+                    "&redirect_uri=$redirectUri")
+            val request = Request(Method.POST, url)
+            val response = client(request)
+
+            if (response.status == Status.OK) {
+                Response(Status.OK).body("Authorization code received: ${response.bodyString()}")
+            } else {
+                logger.error { response.bodyString() }
+                Response(Status.INTERNAL_SERVER_ERROR).body("Authorization code not received")
+            }
         }
     )
 
@@ -45,25 +58,19 @@ fun main() {
 
     val server = securedApp.asServer(SunHttp(8080)).start()
 
-    println("Authorization Server started on port 8000")
-
-    // Perform authorization flow
-    val authorizationRequestUrl = "http://localhost:8000/authorize?state=random_state"
-    println("Open the following URL in your browser to authorize the application:")
-    println(authorizationRequestUrl)
-
+    logger.info("Authorization Server started on port 8000")
+    
     server.shutdownOnExit()
-
     waitForExitSignal()
 }
 
 private fun Http4kServer.shutdownOnExit() {
     Runtime.getRuntime().addShutdownHook(Thread {
-        println("Shutting down...")
+        logger.info("Shutting down...")
 
         stop()
 
-        println("Shutdown complete.")
+        logger.info("Shutdown complete.")
     })
 }
 
